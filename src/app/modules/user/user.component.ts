8b66354266357d717fca095fedda7dcbd0f9a6e4
Osman <osmannamso123@gmail.com>
2020-10-25 20:41:47 +0600

    update: header btns

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  isActiveNotifications = false;
+  isActiveWallet = false;
+
+  @ViewChild('notificationDropdown') notificationDropdown: ElementRef;
+  @ViewChild('walletDropdown') walletDropdown: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener('document:click', ['$event'])
+  onGlobalClick(event): void {
+    if (!this.notificationDropdown.nativeElement.contains(event.target)) {
+      this.isActiveNotifications = false;
+    }
+
+    if (!this.walletDropdown.nativeElement.contains(event.target)) {
+      this.isActiveWallet = false;
+    }
+  }
+
+  toggleNotifications(): void {
+    this.isActiveNotifications = !this.isActiveNotifications;
+  }
+
+  toggleWallet(): void {
+    this.isActiveWallet = !this.isActiveWallet;
+  }
 }
